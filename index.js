0131b1baaaa277930edd39d2dda3b3123b1f11d2
GITNetgains <git@netgains.org>
2023-11-27 11:21:01 +0530

    notification commit

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ server.post("/check", (req, res) => {
 })
 
 server.post("/partner_notification", express.json(), async (req, res) => {
-  const { userid, fcmToken, order_id } = req.body;
+  const { userid, fcmToken, order_id, customerUserId } = req.body;
   try {
     const form = new FormData();
     form.append("userid", userid);
@@ -97,6 +97,61 @@ server.post("/partner_notification", express.json(), async (req, res) => {
       } catch (e) {
         console.log(e.message);
         res.status(500).json({ error: "Error sending FCM notification", "details": error });
+      }
+    }
+
+    const customerForm = new FormData();
+    form.append("userid", userid);
+    const customerResponse = await axios.post(
+      "https://tidasports.com/secure/api/notification/find_fcm_token",
+      form,
+      {
+        headers: {
+          ...form.getHeaders(),
+        },
+      }
+    );
+
+    let customer_fcm_token = response.data.data.fcm_token;
+    
+    if(!Array.isArray(customer_fcm_token)) {
+      customer_fcm_token = [customer_fcm_token];
+    }
+
+    for(let customer_token of customer_fcm_token) {
+      let message = {
+        token: customer_token,
+        notification: {
+          title: "Payment Update",
+          body: "You have received a payment from a Tida customer ",
+        },
+        data: {
+          click_action: "FLUTTER_NOTIFICATION_CLICK",
+          sound: "default",
+          order_id: order_id.toString()
+        }
+      };
+  
+      console.log(message);
+  
+      try {
+        await admin.messaging().send(message)
+          .then((responseFCM) => {
+            // Response is a message ID string.
+            // console.log("FCM notification sent successfully:", responseFCM);
+            logger.info(responseFCM);
+            // res.status(200).json({ message: "FCM notification sent successfully" });
+          }).catch((error) => {
+            console.error("Error sending FCM notification:", error.message);
+            logger.error({
+              "error": error.message,
+              // "response_from_fcm": responseFCM,
+              "response_from_server": response
+            })
+          });
+      } catch (e) {
+        console.log(e.message);
+        // res.status(500).json({ error: "Error sending FCM notification", "details": error });
       }
     }
 
